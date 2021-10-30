96b4e35be34733d780e1fc2b5b7295993c9e9d38
blagowhatnow <58713481+blagowhatnow@users.noreply.github.com>
2021-10-30 11:35:16 -0700

    Add files via upload

---
+++ iupac.py
@@ -0,0 +1,18 @@
+#needs the pubchemlibrary. Use pip install pubchem 
+
+import pubchempy
+
+text_file = open("dataset_cleansed.smi", "r")
+lines = text_file.read().splitlines()
+smiles=lines
+
+iupac_names=[]
+
+for i in smiles:
+  compounds = pubchempy.get_compounds(smiles, namespace='smiles')
+  match = compounds[0]
+  iupac_names.append(match.iupac_names)
+
+
+
+print(iupac_names)
